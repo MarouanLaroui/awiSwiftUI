fc535c04fcc26cbfd6d5affa2d5199812cf28924
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-02-19 11:36:53 +0100

    Initial Commit

---
+++ awiSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  awiSwiftUI
+//
+//  Created by Marouan Laroui  on 19/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ awiSwiftUI/awiSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  awiSwiftUIApp.swift
+//  awiSwiftUI
+//
+//  Created by Marouan Laroui  on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct awiSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
