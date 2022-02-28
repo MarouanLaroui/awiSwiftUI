712477e67291a2933f7f8ecc8f989ef0811c374c
Ophelie <ophelie.amarine@etu.umontpellier.fr>
2022-02-28 18:20:53 +0100

    Label front done

--- awiSwiftUI/Views/LabelManagement/LabelManagement.swift
+++ awiSwiftUI/Views/LabelManagement/LabelManagement.swift
@@ -12,40 +12,93 @@ struct LabelManagement: View {
     
     @State var recipe: Recipe
     @State var ingredients : [Ingredient:Int] = [:]
+    @State private var decrementStock : Bool = false
+    @State private var nbPortions = 1
     
     var etiquette: some View {
         VStack{
-            Text("Etiquette")
-                .font(.title)
-            Text("Ouais ouais les étiquettes ouais")
+            Text("\(recipe.title)")
+                .font(.largeTitle)
+                .bold()
             
-            ForEach(ingredients.sorted(by: ==), id: \.key.id) { ingredient, qtty in
-                HStack{
-                    Text(ingredient.name)
-                    if(ingredient.allergen != nil){
-                        Text(" ("+ingredient.allergen!.name+") ")
-                            .fontWeight(.semibold)
-                            .italic()
+            Text("")
+            
+            Text("Ingrédients : ")
+            VStack (alignment: .leading, spacing: 2.0){
+                ForEach(ingredients.sorted(by: ==), id: \.key.id) { ingredient,qtty in
+                    HStack{
+                        Text(ingredient.name)
+                        if(ingredient.allergen != nil){
+                            Text("("+ingredient.allergen!.name+") ")
+                                .fontWeight(.semibold)
+                                .italic()
+                        }
                     }
                 }
             }
-
+            .padding()
+            
         }
+        .overlay(
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(lineWidth: 2)
+                .foregroundColor(Color.salmon)
+        )
+        
+        .padding()
         .navigationTitle("Etiquette")
     }
+    
+    
     var body: some View{
-        etiquette
-        
-       /* VStack {
-            etiquette
-
-            Button("Save to image") {
-                let image = etiquette.snapshot()
-
-                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        Spacer()
+        VStack {
+            
+            //Nombre de portions vendues
+            HStack{
+                Stepper("\(nbPortions.formatted()) portions vendues",
+                        value: $nbPortions,
+                        in: 1...50,
+                        step: 1)
+                .padding(.horizontal, 80)
+                
             }
+            
+            //Slider décrémentation des stocks
+            HStack{
+                Text("Mettre à jour les stocks")
+                    .foregroundColor(decrementStock ? Color.salmon : .gray)
+                Toggle("Stock", isOn: $decrementStock)
+                    .labelsHidden()
+                    .toggleStyle(SwitchToggleStyle(tint: Color.salmon))
+            }
+            
         }
-        */
         
+        etiquette
+            
+        Spacer()
+            
+        if decrementStock {
+            Text("Les stocks seront décrémentés lors de la capture d'écran.")
+                .bold()
+                .italic()
+                .font(.caption)
+                .opacity(0.5)
+        }
+            
+        Button("Prendre une capture d'écran") {
+           /* let image = etiquette.snapshot()
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            */
+        }
+        .padding()
+        .foregroundColor(.white)
+        .background(Color.salmon)
+        .cornerRadius(40)
+        
+        .padding(.bottom, 80)
+            
     }
+    
 }
